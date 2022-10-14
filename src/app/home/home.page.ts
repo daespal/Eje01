5ef2a8d29780bfa49891db83c0c6fbf18792de6b
daespal <84483704+daespal@users.noreply.github.com>
2022-10-14 01:09:19 -0600

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public photos=[];
 
-  constructor() {}
-
+  constructor() {
+  this.photos = [
+    {
+      name: "Foto 1",
+      description: "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor.",
+      url: "https://picsum.photos/id/237/200/300"
+    },
+    {
+      name: "Foto 2",
+      description: "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor.",
+      url: "https://picsum.photos/id/238/200/300"
+    },
+    {
+      name: "Foto 3",
+      description: "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor.",
+      url: "https://picsum.photos/id/239/200/300"
+    },
+    {
+      name: "Foto 4",
+      description: "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor.",
+      url: "https://picsum.photos/id/240/200/300"
+    }
+  ]
+}
 }
