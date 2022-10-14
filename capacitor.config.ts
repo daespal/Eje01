4f6f1f64a74cd056947d8a8ff265cd436b0c822b
daespal <84483704+daespal@users.noreply.github.com>
2022-10-13 22:16:09 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Eje01_GaleriaImg',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
